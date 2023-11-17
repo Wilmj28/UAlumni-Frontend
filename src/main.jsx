8ffe17a2fb49d40ab45a7d073e5feafcdd05048e
GloodBuster <95661756+GloodBuster@users.noreply.github.com>
2023-11-17 19:39:47 -0400

    Delete app and router in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+//Rutas de ejemplo, se pueden cambiar
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/landing",
+    element: <div>Landing Page</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
