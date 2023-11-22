91c14942d45f25dd82063d3e6a266bd4e586190a
GloodBuster <95661756+GloodBuster@users.noreply.github.com>
2023-11-22 12:12:45 -0400

    Minor fix

--- src/cores/landingpage/LandingPage.jsx
+++ src/cores/landingpage/LandingPage.jsx
@@ -1,17 +1,97 @@
-
 import NavBar from "../../components/Navbar";
+<<<<<<< Updated upstream
 import { Text, Stack, Box, Image, Flex} from '@chakra-ui/react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
 import EgresadosFoto from '../../images/egresados.jpg';
 import EgresadosLanding from '../../images/egresadosLanding.jpg';
 import ReclutadoresLanding from '../../images/reclutadoreslanding.jpg';
+=======
+import {
+  Text,
+  Stack,
+  Box,
+  Image,
+  Flex,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import EgresadosFoto from "../../images/egresados.jpg";
+import EgresadosLanding from "../../images/egresadosLanding.jpg";
+import ReclutadoresLanding from "../../images/reclutadoreslanding.jpg";
+>>>>>>> Stashed changes
 import Footer from "../../components/Footer";
 
 function LandingPage() {
   return (
     <div>
       <NavBar />
+<<<<<<< Updated upstream
       <img src={EgresadosFoto} alt="Egresados" width="100%" height="auto" />
+=======
+      <Box
+        width="100%"
+        height="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          position="relative"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image
+            src={EgresadosFoto}
+            alt="Egresados"
+            width="100%"
+            height="auto"
+          />
+
+          <Box
+            position="absolute"
+            backgroundColor="rgba(255,255,255,0.8)"
+            boxShadow="0 0 0 1px #FFFFFF"
+            padding="8px"
+            backgroundClip="content-box"
+            width={["100%", "90%", "70%", "45%"]}
+            // height={["90%", "90%","70%", "50%"]}
+            zIndex="2"
+          >
+            <Text
+              fontSize={["lg", "lg", "xl", "4xl"]}
+              color="black"
+              textAlign="center"
+              as="b"
+              paddingTop="15px"
+              marginTop="10px"
+              marginBottom="10px"
+              style={{
+                textDecoration: "underline",
+                textDecorationColor: "green",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              UALUMNI
+            </Text>
+            <Text
+              fontSize={["small", "md", "md", "md"]}
+              color="black"
+              textAlign="center"
+              px="20px"
+              paddingBottom="40px"
+            >
+              UAlumni es una plataforma que te ayuda a encontrar el empleo
+              ideal, de acuerdo a tus habilidades y experiencia.En UAlumni
+              encontrarás ofertas de empleo de las mejores empresas del país,
+              así como recursos para prepararte para tu búsqueda laboral.
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+>>>>>>> Stashed changes
       <div
         style={{
           width: "100%",
@@ -24,46 +104,41 @@ function LandingPage() {
         <Text fontSize="4xl" textAlign="center" as="b" paddingTop="40px">
           ¿QUIÉN SOY?
         </Text>
-        <Stack direction="row" justifyContent="space-evenly" paddingTop="40px">
-          <Box
-            boxSize="sm"
-            display="flex"
-            justifyItem="center"
-            flexDirection="column"
-          >
+        <Grid
+          justifyContent="center"
+          paddingTop="40px"
+          gridAutoFlow={["row", "row", "column", "column"]}
+          gap={[10, 10, 6, 6]}
+        >
+          <GridItem boxSize={["", "", "sm", "sm"]}>
             <Image src={EgresadosLanding} alt="egresadosLanding" />
             <Button
               backgroundColor="#007935"
               color="white"
               _hover={{ bg: "#025024" }}
-              size="md"
+              width="100%"
               marginTop="20px"
               paddingTop="10px"
               paddingBottom="10px"
             >
               SOY EGRESADO
             </Button>
-          </Box>
-          <Box
-            boxSize="sm"
-            display="flex"
-            justifyItem="center"
-            flexDirection="column"
-          >
+          </GridItem>
+          <GridItem boxSize={["", "", "sm", "sm"]}>
             <Image src={ReclutadoresLanding} alt="reclutadoresLanding" />
             <Button
               backgroundColor="#007935"
               color="white"
               _hover={{ bg: "#025024" }}
-              size="md"
+              width="100%"
               marginTop="20px"
               paddingTop="10px"
               paddingBottom="10px"
             >
               SOY RECLUTADOR
             </Button>
-          </Box>
-        </Stack>
+          </GridItem>
+        </Grid>
       </div>
 
       <div
@@ -80,9 +155,7 @@ function LandingPage() {
         <Text fontSize="4xl" textAlign="center" as="b" paddingTop="40px">
           ¿DE QUÉ CARRERA ES EL PROFESIONAL QUE BUSCAS?
         </Text>
-        <Stack
-          p={{ base: 4, md: "20 20 10 20" }}
-        >
+        <Stack p={{ base: 4, md: "20 20 10 20" }}>
           <Flex direction="row" justifyContent="center" wrap="wrap">
             <Box my={2} mx={8}>
               <Button
@@ -137,9 +210,7 @@ function LandingPage() {
           </Flex>
         </Stack>
 
-        <Stack
-          p={{ base: 4, md: "20 20 10 20" }}
-        >
+        <Stack p={{ base: 4, md: "20 20 10 20" }}>
           <Flex direction="row" justifyContent="center" wrap="wrap">
             <Box my={2} mx={8}>
               <Button
@@ -189,4 +260,9 @@ function LandingPage() {
     </div>
   );
 }
+<<<<<<< Updated upstream
 export default LandingPage;
+=======
+
+export default LandingPage;
+>>>>>>> Stashed changes
